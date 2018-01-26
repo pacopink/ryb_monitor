0260e480087ffb6ac2981329c9100cd9411d39d6
paco.li <paco.li@ericsson.com>
2018-01-26 23:47:52 +0800

    优化代码

--- scripts/http_hb.py
+++ scripts/http_hb.py
@@ -4,17 +4,14 @@
 import urllib
 import urllib2
 import sys
-import socket
-from urllib2 import HTTPError
 
-url = sys.argv[1] #"http://221.180.247.78/lbs_web/probe/host.jsp"
+url = sys.argv[1]
 
 urllib2.socket.setdefaulttimeout(2) #期待2s内有返回
 if url[0:4].lower() != "http":
     url = "http://"+url
 
-
-def conn():
+def conn(retry=2):
     try:
         code = urllib2.urlopen(url).getcode()
         if code == 200:
@@ -23,23 +20,13 @@ def conn():
         else:
             sys.stdout.write("url[{url}] respond[{code}]".format(url = url, code=code))
             sys.exit(1)
-    except urllib2.URLError,e:
-        sys.stdout.write("url[{url}] exception[{excp}]".format(url = url, excp=e))
-        sys.exit(1)
-    except socket.timeout, e:
-        raise e
     except Exception, e:
-        raise e
-        sys.stdout.write("url[{url}] exception[{excp}]".format(url = url, excp=e))
-        sys.exit(1)
+        if retry>0:
+            return conn(retry-1)
+        else:
+            sys.stdout.write("url[{url}] exception[{excp}]".format(url = url, excp=e))
+            sys.exit(1)
 
 if __name__=="__main__":
-    try:
-        try:
-            conn()
-        except socket.timeout, e:  #如果超时,重试一次
-                conn()
-    except Exception, e:
-        sys.stdout.write("url[{url}] exception[{excp}]".format(url = url, excp=e))
-        sys.exit(1)
+    conn()
 
