eb409ce803a7a321304a08f17bbc32d24016a18e
pacopink <13602887904@163.com>
2020-06-13 16:28:17 +0800

    add SocketLock and CmdExecutor

---
+++ src2/CmdExecutor.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import os
+import threading
+import time
+
+ERR_EXECUTE=-999
+
+def exec_cmd(command, timeout):
+    """call shell-command and either return its output or kill it 
+    if it doesn't normally exit within timeout seconds and return None"""
+    import subprocess, datetime, os, time, signal
+    cmd = command.split(" ")
+    start = datetime.datetime.now()
+    try:
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        ###避免buffer满堵塞pipe，这里使用一个独立的线程来接收标准输出和标准出错###
+        output = [None, None]
+        def gatherOutput():
+            output[0] = process.stdout.read()
+            output[1] = process.stderr.read()
+        t = threading.Thread(target=gatherOutput)
+        t.start()
+
+        while process.poll() is None:
+            time.sleep(0.1)
+            now = datetime.datetime.now()
+            if (now - start).seconds > timeout:
+                os.kill(process.pid, signal.SIGKILL)
+                os.waitpid(-1, os.WNOHANG)
+                t.join(0.1)
+                return (2, "TIMEOUT", "TIMEOUT")
+        t.join(2) #wait for output collection
+        return (process.returncode, output[0], output[1])
+    except Exception,e :
+        return (ERR_EXECUTE, e.__str__(), e.__str__())
+
+
+def get_output_lines(cmd):
+    '''get huge volumn of outputs'''
+    with os.popen(cmd) as p:
+        for l in p:
+            yield l
+
+def generate_script_file(path, cmd, sub_cmds):
+    with open(path, 'w') as f:
+        f.write(cmd)
+        f.write("<<__EOF__\n")
+        f.write(sub_cmds)
+        if sub_cmds[-1:] != "\n":
+            f.write("\n")
+        f.write("__EOF__")
+        f.write("\n")
+
+
+if __name__=='__main__':
+    import sys
+    from SocketLock import *
+    @withLockWait
+    def do():
+        FILE="/tmp/xxx.sh"
+        print exec_cmd("rm -rf "+FILE, 10)
+        generate_script_file(FILE, "/opt/bin/cliss", "aaa\nbbbb\ncccc\ncommit\nexit\n")
+        for l in get_output_lines("cat "+FILE):
+            sys.stderr.write(l)
+        print exec_cmd("sh "+FILE, 10)
+    do()
+
+
+
+
---
+++ src2/SocketLock.py
@@ -0,0 +1,128 @@
+#!/bin/env python
+#coding:utf8
+'''通过打开一个本地的UDP端口,来检测是否有相同的程序在运行'''
+
+from socket import *
+
+# 指定一个UDP端口号用于互斥
+PORT = 64444
+
+class LockTimeoutException(Exception):
+    '''LockTimeoutException'''
+    def __str__(self):
+        return 'LockTimeoutException'
+
+class LockAcquireFailException(Exception):
+    '''lock acquire failed'''
+    def __str__(self):
+        return 'LockAcquireFailException'
+
+class LockNotHolderToReleaseException(Exception):
+    '''if I am not the lock holder and a release is revoked'''
+    def __str__(self):
+        return 'LockAcquireFailException'
+
+class SocketLock:
+    def __init__(self, port=PORT):
+        self.addr = ("127.0.0.1", port)
+        self.s = None
+
+    def acquire_nonblock(self):
+        '''acuiqre lock without blocking'''
+        if self.s is not None:
+            return
+        try:
+            self.s = socket(AF_INET, SOCK_DGRAM)
+            # not allow socket reuse
+            self.s.setsockopt(SOL_SOCKET,SO_REUSEADDR, 0)
+            self.s.bind(self.addr)
+            return
+        except Exception,e:
+            self.s = None
+            #log_debug(e)
+            raise LockAcquireFailException()
+
+    def acquire_block(self, tick=200):
+        '''acuiqre lock if failed sleep 0.1 seconds and retry for 200 times'''
+        import time
+        print_flag = False
+        while True:
+            try:
+                self.acquire_nonblock()
+                return
+            except LockAcquireFailException:
+                pass
+            tick -= 1
+            if tick<0:
+                break
+            if not print_flag:
+                # only print this line once
+                print "failed to acquire lock, waiting ..."
+                print_flag = True
+            try:
+                time.sleep(0.1)
+            except Exception:
+                break
+        raise LockTimeoutException()
+
+    def release(self):
+        '''release the holding lock'''
+        if self.s is not None:
+            self.s.close()
+            self.s = None
+        else:
+            raise LockNotHolderToReleaseException()
+
+def withLockWait(fun):
+    '''decorator of function with blocking lock'''
+    def wrapper(*args, **kargs):
+        lock = SocketLock()
+        lock.acquire_block()
+        ret = fun(*args, **kargs)
+        lock.release()
+        return ret
+    return wrapper
+
+def withLock(fun):
+    '''decorator of function with nonblocking lock'''
+    def wrapper(*args, **kargs):
+        lock = SocketLock()
+        lock.acquire_nonblock()
+        ret = fun(*args, **kargs)
+        lock.release()
+        return ret
+    return wrapper
+        
+if __name__=="__main__":
+    import threading
+    import time
+    from Logger import *
+
+    @withLockWait
+    def do(name, sleep):
+        log_info("enter: "+ name)
+        time.sleep(sleep)
+        log_info("exit: "+ name)
+        time.sleep(0.01)
+
+    def doIt(name, sleep):
+        try:
+            do(name, sleep)
+        except Exception,e:
+            log_error(name+":"+str(e))
+
+    # 并发若干个线程，争抢锁资源, 按20s的等待时常，其中有一个必然抢不到
+    tt = map(lambda x:threading.Thread(target=doIt, args=x), 
+            [("thread-1", 5), 
+                ("thread-2", 5), 
+                ("thread-3", 6), 
+                ("thread-4", 6), 
+                ("thread-5", 5)]
+            )
+    map(lambda x:x.start(), tt)
+    map(lambda x:x.join(), tt)
+
+
+
+
+
